a027bdeb0796cb38fbd2003416b0ad5eb5e2889a
Shawn Kim <shawnnicolekim@gmail.com>
2020-09-24 16:02:35 -0700

    Fixed syntax for schemas

--- database/schemas.sql
+++ database/schemas.sql
@@ -2,11 +2,11 @@ CREATE DATABASE IF NOT EXISTS ProductWishlist;
 
 USE ProductWishlist;
 
-CREATE TABLE IF NOT EXISTS wishes {
+CREATE TABLE IF NOT EXISTS wishes (
   id int NOT NULL AUTO_INCREMENT,
   name varchar(320) NOT NULL,
   metal varchar(5) NOT NULL,
   price int NOT NULL,
   imageUrl varchar(320) NOT NULL,
   PRIMARY KEY (id)
-}
+)
